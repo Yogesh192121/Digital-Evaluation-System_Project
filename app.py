02ac80d3668892e2d2c064efdb632ee0252f8df7
Yogesh Ishwar Uprikar <yogeshuprikar037@gmail.com>
2026-04-23 03:47:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1910,6 +1910,8 @@ def logout():
     return redirect("/")
 
 # ================= RUN =================
-if __name__ == "__main__":
 import os
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
